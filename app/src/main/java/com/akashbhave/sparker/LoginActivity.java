d672209892f165be601623723e9254ed48c77088
Akash Bhave <akashbbhave@gmail.com>
2017-02-21 21:09:41 -0500

    Initial commit: Contains the basic sign up screen (SignupActivity) and the login screen (LoginActivity) for existing users. Firebase is the planned backend for the application. Basic XML, no customization of themes or colors.

---
+++ app/src/main/java/com/akashbhave/sparker/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.akashbhave.sparker;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public void sendToSignupActivity(View view) {
+        Intent signupActivityIntent = new Intent(this, SignupActivity.class);
+        startActivity(signupActivityIntent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Locks the orientation to portrait
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
